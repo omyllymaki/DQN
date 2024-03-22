2e6cef76c072b4d1ccb26ed8d7364cce20f5bb1d
Ossi Myllymäki <omyllymaki@gmail.com>
2024-03-22 16:40:18 +0200

    More panes and grid shape

--- src/samples/run_multiple.sh
+++ src/samples/run_multiple.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
 
+# Function to split panes
+make_grid() {
+    local h="$1"
+    local v="$2"
+
+    # Horizontal split
+    if [ "$h" -gt 1 ]; then
+        for (( i=1; i<h; i++ )); do
+            tmux split-window -h
+        done
+        tmux select-layout even-horizontal
+    fi
+
+    # Vertical split
+    if [ "$v" -gt 1 ]; then
+        tmux select-pane -t 0
+        v_total=$(tmux display-message -p '#{pane_height}')
+        v_size=$(( v_total /  v ))
+        for (( i=0; i<h; i++ )); do
+            pane=$(( i * v ))
+            tmux select-pane -t $pane
+            for (( j=1; j<v; j++ )); do
+                tmux split-window -l $v_size -v
+                tmux select-pane -t $pane
+            done
+        done
+        tmux select-pane -t 0
+    fi
+}
+
 tmux new-session -d -s dqn_session
 
-tmux split-window -t dqn_session -v
-tmux split-window -t dqn_session -v
-tmux split-window -t dqn_session -v
-tmux split-window -t dqn_session -v
-tmux select-layout -t dqn_session even-vertical
+h=2
+v=4
+make_grid "$h" "$v"
 
-for ((i=0; i<=3; i++)); do
+for ((i=0; i<=7; i++)); do
     tmux select-pane -t $i
     tmux send-keys -t dqn_session "source venv/bin/activate" C-m
     tmux send-keys -t dqn_session "export PYTHONPATH=$PWD" C-m
 done
 
-command_prefix="python src/samples/sample_test_bench.py --device cpu --method priority_sampling --n_episodes 200 --n_runs 2"
-update_rates=("0.05" "0.01" "0.1")
+command_prefix="python src/samples/sample_test_bench.py --device cuda --method priority_sampling --n_episodes 1000 --n_runs 20"
+update_rates=("0.005" "0.01" "0.1")
 for i in "${!update_rates[@]}"; do
     tmux select-pane -t "$i"
     tmux send-keys -t dqn_session "$command_prefix --target_network_update_rate ${update_rates[$i]}" C-m
